b085d43c4ad475e55c6ff1f1d8a1fff4a84ec887
PonzaMatteo <matteoponza@gmail.com>
2018-11-27 18:11:51 +0100

    clean version for start branching

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express'); // this will use express to handle request
 const app = express();
-const morgan = require('morgan'); //middleware for authentication
+// const morgan = require('morgan'); //middleware for authentication
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const exercises = require('./api/routes/exercises');
-const users = require('./api/routes/users');
+// const exercises = require('./api/routes/exercises');
+// const users = require('./api/routes/users');
 
 // DB connection
-mongoose.connect('mongodb://nathaniellee:'
-    + process.env.MONGO_ATLAS_PSW  +'@se2nathaniellee-shard-00-00-kcdnu.gcp.mongodb.net:27017,se2nathaniellee-shard-00-01-kcdnu.gcp.mongodb.net:27017,se2nathaniellee-shard-00-02-kcdnu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=SE2Nathaniellee-shard-0&authSource=admin&retryWrites=true',
-    {
-        useMongoClient: true
-    });
-mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://nathaniellee:'
+//     + process.env.MONGO_ATLAS_PSW  +'@se2nathaniellee-shard-00-00-kcdnu.gcp.mongodb.net:27017,se2nathaniellee-shard-00-01-kcdnu.gcp.mongodb.net:27017,se2nathaniellee-shard-00-02-kcdnu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=SE2Nathaniellee-shard-0&authSource=admin&retryWrites=true',
+//     {
+//         useMongoClient: true
+//     });
+// mongoose.Promise = global.Promise;
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false})); //this would parse urlencoded requets, without rich-extended options (false)
 app.use(bodyParser.json()); // this will extract json data from requests
 
@@ -38,9 +38,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// Set routes which should handle requests, these method forwards directly to js file with corrispondent method
-app.use("/exercises", exercises);
-app.use('/users', users);
+// // Set routes which should handle requests, these method forwards directly to js file with corrispondent method
+// app.use("/exercises", exercises);
+// app.use('/users', users);
 
 // if the server reach that line, none of the routes above was able to process the request, so i should send an error message
 app.use((req, res, next) => {
